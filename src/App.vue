5085f47895b4b57b2440c0a0361e3088a0c9cc48
nowij <nowijdl@gmail.com>
2023-07-21 13:51:44 +0900

    검색창 추가

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,13 @@
   <!-- 줄여쓰기 v-on:   = @      -->
   <div class="container">
     <h2>To-Do LIST</h2>
+    <input class="form-control" type="text" v-model="searchText" placeholder="Search" >
+    <hr />
     <TodoSimpleForm @add-todo="addTodo"/>
-    <TodoList :todoList="todoList"
+    <div v-if="!filteredTodoList.length">
+      There is nothing to display
+    </div>
+    <TodoList :todoList="filteredTodoList"
       @toggle-todo="toggleTodo"
       @delete-todo="deleteTodo"/>
 </div>
@@ -12,7 +17,7 @@
 </template>
 
 <script>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
 import TodoList from './components/TodoList.vue';
 
@@ -39,11 +44,24 @@ export default {
       todoList.value[index].completed = !todoList.value[index].completed;
     }
 
+    const searchText = ref('', computed);
+    const filteredTodoList = computed(() => {
+      if (searchText.value) {
+        return todoList.value.filter(todo => {
+          return todo.subject.includes(searchText.value);
+        });
+      }
+
+      return todoList.value;
+    });
+
     return {
       todoList,
       addTodo,
       deleteTodo,
-      toggleTodo
+      toggleTodo,
+      searchText,
+      filteredTodoList
     };
   }
 }
